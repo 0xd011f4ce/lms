ea350487cce0abdae4c625ddfd117e47c3bdd3df
lain <lyx@lyxincyberia.org>
2024-08-31 19:38:33 -0500

    added routes, and controllers for the admin, and instructor dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InstructorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::get("/admin/dashboard", [AdminController::class, "AdminDashboard"])->name("admin.dashboard");
+Route::get("/instruction/dashboard", [InstructorController::class, "InstructorDashboard"])->name("instructor.dashboard");
